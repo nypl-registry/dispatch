b0e22a1678ee1998c97e8d9ca06b022ec4d302ba
Matt Miller <thisismattmiller@gmail.com>
2016-04-07 20:00:00 -0400

    terms job and script

--- jobs/serialize_terms.js
+++ jobs/serialize_terms.js
@@ -1,61 +1,49 @@
 var serializeTerms = require('nypl-registry-serialization-terms')
 
-serializeTerms.shadowcatSerializeTerms(() => {
-  console.log('shadowcatSerializeTerms done')
-  process.exit(0)
-})
-// if (process.argv[2] && process.argv[2].toLowerCase() === 'shadowcatviaf') {
-//   console.log('shadowcatSerializeViafAgents')
-//   serializeAgents.shadowcatSerializeViafAgents(() => {
-//     console.log('shadowcatSerializeViafAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'shadowcatnonviaf') {
-//   console.log('shadowcatSerializeNonViafAgents')
-//   serializeAgents.shadowcatSerializeNonViafAgents(() => {
-//     console.log('shadowcatSerializeNonViafAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'archivescollections') {
-//   console.log('archivesSerializeCollectionAgents')
-//   serializeAgents.archivesSerializeCollectionAgents(() => {
-//     console.log('archivesSerializeCollectionAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'archivescomponents') {
-//   console.log('archivesSerializeComponentsAgents')
-//   serializeAgents.archivesSerializeComponentsAgents(() => {
-//     console.log('archivesSerializeComponentsAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'mmscollections') {
-//   console.log('mmsSerializeCollectionsAgents')
-//   serializeAgents.mmsSerializeCollectionsAgents(() => {
-//     console.log('mmsSerializeCollectionsAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'mmscontainers') {
-//   console.log('mmsSerializeContainerAgents')
-//   serializeAgents.mmsSerializeContainerAgents(() => {
-//     console.log('mmsSerializeContainerAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'mmsitems') {
-//   console.log('mmsSerializeItemsAgents')
-//   serializeAgents.mmsSerializeItemsAgents(() => {
-//     console.log('mmsSerializeItemsAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'tmsobjects') {
-//   console.log('tmsSerializeObjectsAgents')
-//   serializeAgents.tmsSerializeObjectsAgents(() => {
-//     console.log('tmsSerializeObjectsAgents done')
-//     process.exit(0)
-//   })
-// } else if (process.argv[2] && process.argv[2].toLowerCase() === 'enumerate') {
-//   console.log('enumerateAgents')
-//   serializeAgents.enumerateAgents(() => {
-//     console.log('enumerateAgents done')
-//     process.exit(0)
-//   })
-// }
+if (process.argv[2] && process.argv[2].toLowerCase() === 'shadowcat') {
+  console.log('shadowcatSerializeTerms')
+  serializeTerms.shadowcatSerializeTerms(() => {
+    console.log('shadowcatSerializeTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'archivescollections') {
+  console.log('archivesCollectionsSerializeTerms')
+  serializeTerms.archivesCollectionsSerializeTerms(() => {
+    console.log('archivesCollectionsSerializeTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'archivescomponents') {
+  console.log('archivesComponentsSerializeTerms')
+  serializeTerms.archivesComponentsSerializeTerms(() => {
+    console.log('archivesComponentsSerializeTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'mmscollections') {
+  console.log('mmsCollectionsSerializeTerms')
+  serializeTerms.mmsCollectionsSerializeTerms(() => {
+    console.log('mmsCollectionsSerializeTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'mmscontainers') {
+  console.log('mmsContainersSerializeTerms')
+  serializeTerms.mmsContainersSerializeTerms(() => {
+    console.log('mmsContainersSerializeTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'mmsitems') {
+  console.log('mmsItemsSerializeTerms')
+  serializeTerms.mmsItemsSerializeTerms(() => {
+    console.log('mmsItemsSerializeTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'enumerate') {
+  console.log('enumerateTerms')
+  serializeTerms.enumerateTerms(() => {
+    console.log('enumerateTerms done')
+    process.exit(0)
+  })
+} else if (process.argv[2] && process.argv[2].toLowerCase() === 'prepare') {
+  serializeTerms.prepareTerms(() => {
+    process.exit(0)
+  })
+}
---
+++ scripts/serialize_terms.sh
@@ -0,0 +1,8 @@
+node jobs/serialize_terms.js prepare
+node jobs/serialize_terms.js shadowcat
+node jobs/serialize_terms.js archivescollections
+node jobs/serialize_terms.js archivescomponents
+node jobs/serialize_terms.js mmscollections
+node jobs/serialize_terms.js mmscontainers
+node jobs/serialize_terms.js mmsitems
+node jobs/serialize_terms.js enumerate
